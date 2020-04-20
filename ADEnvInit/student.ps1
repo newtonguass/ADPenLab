f31fda30778be2a1921d0c3d3570e1a6ec486346
Howard <newtonguass@gmail.com>
2020-04-21 03:17:40 +0800

    add computer to domain

--- ADEnvInit/student.ps1
+++ ADEnvInit/student.ps1
@@ -2,5 +2,16 @@ try{
     $name = (Get-NetAdapter | select Name).Name
     Disable-NetAdapterBinding –InterfaceAlias $name –ComponentID ms_tcpip6
 }catch{
-
+    Write-Host "Fail to disable IPv6"
 }
+<#
+try{
+    $domain = "hackcollege.tw"
+    $password = "Hackcollege@2020" | ConvertTo-SecureString -asPlainText -Force
+    $username = "$domain\student"
+    $credential = New-Object System.Management.Automation.PSCredential($username,$password)
+    Add-Computer -DomainName $domain -Credential $credential -Restart
+}catch{
+    Write-Host "Fail to disable IPv6"
+}
+#>
